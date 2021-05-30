3a7d53457a70d45def652f0a2abb0cd7a59e66a1
rocunha09 <ro.cunha00)@gmail.com>
2021-05-30 15:53:09 -0300

    implementando rotas e agrupando para área privada, agrupando e nomeando rotas.

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'PrincipalController@principal');
+//rotas com acesso público
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/login', function(){return 'login';})->name('site.login');
 
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
+//rotas agrupadas dentro de um prefixo para acesso privado.
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function(){return 'clientes';})->name('app.clientes');
+    Route::get('/fornecedores', function(){return 'fornecedores';})->name('app.fornecedores');
+    Route::get('/produtos', function(){return 'produtos';})->name('app.produtos');
+});    
 
-Route::get('/contato', 'ContatoController@contato');
+//as rotas nomeadas facilitam o uso do helper disponibilizado pelo framework na lógica da apliçaão, como um "apelido".
+//desta forma nosso menu de navegação pode ficar assim:
+
+//antes:
+//<a href="/">Principal</a>
+//depois
+//<a href="{{route('site.index')}}">Principal</a>
+
+//o grande benefício é que se a "assinatura" da rota mudar, não temos uma dependência direta gerando alteração em várias partes do código, mas apenas em um ponto.
